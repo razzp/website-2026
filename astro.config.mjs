8efc6ff1979aa966832f525113261f957d7d57a0
razzp <64423130+razzp@users.noreply.github.com>
2026-07-08 23:44:42 +0100

    Get Tailwind up and running

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     prefetch: true,
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
