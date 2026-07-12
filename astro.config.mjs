5a8268fc10be65c32584f4871dcd7453773b8fd4
razzp <64423130+razzp@users.noreply.github.com>
2026-07-12 23:25:52 +0100

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     prefetch: true,
-
     vite: {
         plugins: [tailwindcss()],
     },
